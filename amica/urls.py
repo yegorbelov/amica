733b7cfbd0c916fd51c5031003587ed9c27eb439
Egor Belov <ebelov54@gmail.com>
2026-01-23 23:25:13 +0300

    feat: add Nginx service to Docker Compose for media and protected files handling

--- amica/urls.py
+++ amica/urls.py
@@ -17,6 +17,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.PROTECTED_MEDIA_URL, document_root=settings.PROTECTED_MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
