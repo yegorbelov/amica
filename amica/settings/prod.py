a3515bbf0b4fc583117a496735aeb4fdeb299d81
Egor Belov <ebelov54@gmail.com>
2026-01-24 02:28:42 +0300

    fix: cors

--- amica/settings/prod.py
+++ amica/settings/prod.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://chillin.nekotyan2d.ru",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://chillin.nekotyan2d.ru",
+]
+
+CSP_IMG_SRC = ("'self'", "data:", "blob:", "https://chillin.nekotyan2d.ru")
+CSP_CONNECT_SRC = ("'self'", "wss://chillin.nekotyan2d.ru")
