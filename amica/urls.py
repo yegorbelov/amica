bfea00977540c28086953805eb1a501eadaf8521
Egor Belov <ebelov54@gmail.com>
2026-01-30 18:58:52 +0300

    fix: comment out robots.txt path in urls.py; add user agent check in nginx config

--- amica/urls.py
+++ amica/urls.py
@@ -6,10 +6,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "robots.txt",
-        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
-    ),
+    # path(
+    #     "robots.txt",
+    #     TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    # ),
     path("api/", include("apps.Site.urls")),
     path("api/", include("apps.accounts.urls")),
     path("api/media_files/", include("apps.media_files.urls")),
